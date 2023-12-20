5ebc57110f62170ebaa3d5b16cad731852c94508
enocquet <emmanuelnocquet8@gmail.com>
2023-12-20 15:35:30 +0100

    add api_dns_name to passed parameters

--- main.tf
+++ main.tf
@@ -33,4 +33,5 @@ module "cosmotech-tenant" {
   kubernetes_version     = var.kubernetes_version
   loadbalancer_ip        = var.loadbalancer_ip
   resource_group         = var.resource_group
+  api_dns_name           = var.api_dns_name
 }
