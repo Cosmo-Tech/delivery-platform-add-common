1c84b32008a76a1c73a4bbedbd6e7b7569d76631
enocquet <emmanuelnocquet8@gmail.com>
2023-12-26 18:20:37 +0100

    fix naming in output value for publicip

--- outputs.tf
+++ outputs.tf
@@ -11,5 +11,5 @@ output "out_platform_sp_name" {
 }
 
 output "out_platform_public_ip" {
-  value = module.cosmotech-common.out_public_ip_name
+  value = module.cosmotech-common.out_platform_public_ip
 }
